01271765922bdb4c853563979821cdabad797505
Muhammad Iman Zulhakim B. Mohd Zulkhori 288430 <imanzulhakim0831@gmail.com>
2024-02-09 01:35:00 +0800

    interface gui

---
+++ Project/Sentiment Analysis/static/js/main.js
@@ -0,0 +1,23 @@
+(function($) {
+    "use strict";
+    $('.input100').each(function() {
+        $(this).on('blur', function() {
+            if ($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            } else {
+                $(this).removeClass('has-val');
+            }
+        })
+    })
+
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+        $(thisAlert).addClass('alert-validate');
+    }
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+        $(thisAlert).removeClass('alert-validate');
+    }
+}
+)(jQuery);
